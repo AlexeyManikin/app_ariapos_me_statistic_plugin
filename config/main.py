acf1d482e17722f126966c8a24e75ad469802bbe
alexeymanikin <alexey@manikin.dev>
2024-08-17 22:20:52 +0100

    add config file

---
+++ config/main.py
@@ -0,0 +1,12 @@
+__author__ = 'alexeyymnaikin'
+
+import os
+
+# Default logger
+CURRENT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))+'/../')
+
+MYSQL_HOST = 'host'
+MYSQL_PORT = 3306
+MYSQL_USER = 'user'
+MYSQL_PASSWD = 'passwd'
+MYSQL_DATABASE = 'baseOD'
